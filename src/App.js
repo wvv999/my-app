c268af4b4d3b76f51d5271f9d1fb60c6069c3196
Sabrina <bina.oliver4@gmail.com>
2023-05-06 10:35:08 -0300

    Fixing error fieldset

--- src/App.js
+++ src/App.js
@@ -4,38 +4,36 @@ import './style.css';
 function App() {
   return (
     <div className="App">
-      
-        
-          <div className="context">
-            <div className="logo">
-              <img src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="Facebook Logo" />
-            </div>
-            <div className="container flex">
-              <span>Entrar no Facebook</span>
-              <Form className="form flex">
-                <Form.Group controlId="formBasicEmail" className="formgroup">
-                  <Form.Control type="email" placeholder="Email ou telefone" />
-                </Form.Group>
+      <div className="context">
+        <div className="logo">
+          <img src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="Facebook Logo" />
+        </div>
+        <div className="container flex">
+          <span>Entrar no Facebook</span>
+          <Form className="form flex">
+            <Form.Group controlId="formBasicEmail" className="formgroup">
+              <Form.Control type="email" placeholder="Email ou telefone" />
+            </Form.Group>
 
-                <Form.Group controlId="formBasicPassword" className="formgroup">
-                  <Form.Control type="password" placeholder="Senha" />
-                </Form.Group>
+            <Form.Group controlId="formBasicPassword" className="formgroup">
+              <Form.Control type="password" placeholder="Senha" />
+            </Form.Group>
 
-                <Button variant="primary" type="submit">
-                  Entrar
-                </Button>
-              </Form>
-              <a href="#">Esqueceu a conta?</a>
+            <Button variant="primary" type="submit" className='login'>
+              Entrar
+            </Button>
+          </Form>
+          <a href="#">Esqueceu a conta?</a>
 
-              <Form method="post" action="#" className="form">
-                    <legend><span></span>ou<span></span></legend>
-              </Form>
-            <button type="submit" class="nc" > Criar Conta</button>
-            </div>
+          <Form method="post" action="#" className="form">
+            <fieldset>
+              <legend>ou</legend>
+            </fieldset>
+          </Form>
+        <Button type="submit" className="nc"> Criar Conta</Button>
+        </div>
 
-          </div>
-        
-      
+      </div>
     </div>
   );
 }
