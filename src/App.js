edb36fb2f1322a79ca4f8ec0cee2279bba8e22dd
wvv999 <wesley3viana@gmail.com>
2023-05-06 10:00:10 -0300

    react_face

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Button, Form } from 'react-bootstrap';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Wesley :)</h1>
-      </header>
+      
+        
+          <div className="context">
+            <div className="logo">
+              <img src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="Facebook Logo" />
+            </div>
+            <div className="container flex">
+              <span>Entrar no Facebook</span>
+              <Form className="form flex">
+                <Form.Group controlId="formBasicEmail" className="formgroup">
+                  <Form.Control type="email" placeholder="Email ou telefone" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword" className="formgroup">
+                  <Form.Control type="password" placeholder="Senha" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                  Entrar
+                </Button>
+              </Form>
+              <a href="#">Esqueceu a conta?</a>
+
+              <Form method="post" action="#" className="form">
+                    <legend><span></span>ou<span></span></legend>
+              </Form>
+            <button type="submit" class="nc" > Criar Conta</button>
+            </div>
+
+          </div>
+        
+      
     </div>
   );
 }
